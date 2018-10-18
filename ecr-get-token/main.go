77bf4ebbc244b395d4115aaf64cb8e92e2dbd48e
Mo O'Rourke <morourke@ziprecruiter.com>
2018-10-18 00:36:18 -0700

    add formatting options

--- ecr-get-token/main.go
+++ ecr-get-token/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"bytes"
 	"encoding/base64"
+	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"net/http"
@@ -16,9 +18,10 @@ import (
 
 func main() {
 	var err error
-	var region, accountId string
+	var region, accountId, format string
 	flag.StringVar(&region, "region", "", "AWS region")
 	flag.StringVar(&accountId, "account", "", "Account/Registry ID")
+	flag.StringVar(&format, "format", "porcelain", "Output format")
 	flag.Parse()
 
 	if len(region) == 0 {
@@ -33,18 +36,26 @@ func main() {
 			Timeout: time.Minute,
 		},
 	}
-	sess := session.Must(session.NewSession(&config))
-	client := ecr.New(sess)
-	var token *string
-	token, err = getEcrToken(client, accountId)
-	if err != nil {
+	var sess *session.Session
+	if sess, err = session.NewSession(&config); err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(2)
 	}
-	fmt.Fprint(os.Stdout, *token)
+	client := ecr.New(sess)
+	var token *string
+	if token, err = getToken(client, accountId); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(2)
+	}
+	var out string
+	if out, err = formatToken(*token, format); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(2)
+	}
+	fmt.Fprint(os.Stdout, out)
 }
 
-func getEcrToken(c *ecr.ECR, accountId string) (*string, error) {
+func getToken(c *ecr.ECR, accountId string) (*string, error) {
 	req := &ecr.GetAuthorizationTokenInput{}
 	if len(accountId) != 0 {
 		req.RegistryIds = append(req.RegistryIds, &accountId)
@@ -56,15 +67,53 @@ func getEcrToken(c *ecr.ECR, accountId string) (*string, error) {
 	if len(resp.AuthorizationData) == 0 {
 		return nil, fmt.Errorf("No authorization tokens found")
 	}
-	var creds []byte
-	if creds, err = base64.StdEncoding.DecodeString(*resp.AuthorizationData[0].AuthorizationToken); err != nil {
-		return nil, err
+	return resp.AuthorizationData[0].AuthorizationToken, nil
+}
+
+func parseToken(token string) (user string, pass string, err error) {
+	var b []byte
+	if b, err = base64.StdEncoding.DecodeString(token); err != nil {
+		return
 	}
-	// creds are user:password
-	i := bytes.Index(creds, []byte(":"))
-	if i < 0 {
-		return nil, fmt.Errorf("Error parsing token")
+	parts := bytes.Split(b, []byte(":"))
+	if len(parts) != 2 {
+		err = errors.New("Error parsing credentials")
+		return
 	}
-	// return the password
-	return aws.String(string(creds[i+1:])), nil
+	return string(parts[0]), string(parts[1]), nil
+}
+
+func formatToken(token string, format string) (out string, err error) {
+	if format == "base64" {
+		return token, nil
+	}
+	var user, pass string
+	if user, pass, err = parseToken(token); err != nil {
+		return
+	}
+	switch format {
+	case "porcelain":
+		out = fmt.Sprintf("%s %s\n", user, pass)
+	case "username":
+		out = user
+	case "password":
+		out = pass
+	case "shell":
+		out = fmt.Sprintf("USERNAME=%s\nPASSWORD=%s\n", user, pass)
+	case "json":
+		m := map[string]string{
+			"username": user,
+			"password": pass,
+		}
+		var b []byte
+		if b, err = json.Marshal(m); err != nil {
+			return
+		}
+		out = string(b)
+	case "yaml":
+		out = fmt.Sprintf("username: \"%s\"\npassword: \"%s\"\n", user, pass)
+	default:
+		err = errors.New("Unsupported format")
+	}
+	return
 }
